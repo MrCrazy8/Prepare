21f271781233b8fb7d26843a36243f8981d7134e
Naategh <manamtabeshekan@gmail.com>
2019-05-14 13:58:27 +0430

    Add files via upload

---
+++ core/functions.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+class Functions:
+
+    def Programming(self):
+        print("\033[91m1)\033[0m Lamp(php7,apache2,mysql)\t\033[91m4)\033[0m VScode")
+        print("\033[91m2)\033[0m Sublime text\t\t\t\033[91m5)\033[0m Eclips")
+        print("\033[91m3)\033[0m Atom ide\t\t\t\033[91m6)\033[0m Android Studio")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Office(self):
+        print("\033[91m1)\033[0m Gedit")
+        print("\033[91m2)\033[0m LibreOffice Collection")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Internet(self):
+        print("\033[91m1)\033[0m Chrome\t\t\033[91m3)\033[0m Chromium")
+        print("\033[91m2)\033[0m Firefox\t\t\033[91m4)\033[0m Tor browser")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Graphic(self):
+        print("\033[91m1)\033[0m Gimp\t\t\t\033[91m3)\033[0m Krita")
+        print("\033[91m2)\033[0m Inkscape\t\t\033[91m4)\033[0m MyPaint")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Usefull(self):
+        print("\033[91m1)\033[0m Dropbox\t\t\t\033[91m7)\033[0m Lantern")
+        print("\033[91m2)\033[0m Vlc\t\t\t\t\033[91m8)\033[0m Thunderbird")
+        print("\033[91m3)\033[0m Wine\t\t\t\t\033[91m9)\033[0m Ngrok")
+        print("\033[91m4)\033[0m Remmina\t\t\t\033[91m10)\033[0m Xtreme downloader")
+        print("\033[91m5)\033[0m SimpleScreenRecorder\t\t\033[91m11)\033[0m Audacious")
+        print("\033[91m6)\033[0m Gitkraken\t\t\t\033[91m12)\033[0m Pidgin")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Social(self):
+        print("\033[91m1)\033[0m Telegram\t\t\t\033[91m3)\033[0m Skype")
+        print("\033[91m2)\033[0m Whats app\t\t\t\033[91m4)\033[0m Choqok")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Etc(self):
+        print("\033[91m1)\033[0m Cumulus\t\t\t\033[91m5)\033[0m Photorec")
+        print("\033[91m2)\033[0m Kbackup\t\t\t\033[91m6)\033[0m Anbox")
+        print("\033[91m3)\033[0m Filezilla\t\t\t\033[91m7)\033[0m Virtualbox")
+        print("\033[91m4)\033[0m Dtrx\t\t\t\t\033[91m8)\033[0m Cowsay")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
+
+    def Hacking(self):
+        print("\033[91m1)\033[0m Reacon-ng\t\t\t\033[91m16)\033[0m Aircrack-ng")
+        print("\033[91m2)\033[0m Nmap\t\t\t\t\033[91m17)\033[0m Mdk3")
+        print("\033[91m3)\033[0m Wafw00f\t\t\t\033[91m18)\033[0m Reaver")
+        print("\033[91m4)\033[0m Nikto\t\t\t\033[91m19)\033[0m Armitage")
+        print("\033[91m5)\033[0m BurpSuite\t\t\t\033[91m20)\033[0m Metasploit")
+        print("\033[91m6)\033[0m Sqlmap\t\t\t\033[91m21)\033[0m Beef")
+        print("\033[91m7)\033[0m WPScan\t\t\t\033[91m22)\033[0m Searchsploit")
+        print("\033[91m8)\033[0m JoomScan\t\t\t\033[91m23)\033[0m SEToolkit")
+        print("\033[91m9)\033[0m CMSmap\t\t\t\033[91m24)\033[0m Ettercap")
+        print("\033[91m10)\033[0m Dirb\t\t\t\033[91m25)\033[0m Macchanger")
+        print("\033[91m11)\033[0m Crunch\t\t\t\033[91m26)\033[0m Wireshark")
+        print("\033[91m12)\033[0m John\t\t\t\033[91m27)\033[0m Sslstrip")
+        print("\033[91m13)\033[0m Hydra\t\t\t\033[91m28)\033[0m Weevely")
+        print("\033[91m14)\033[0m Hashcat\t\t\t\033[91m29)\033[0m ProxyChains")
+        print("\033[91m15)\033[0m Hash-Identifier\t\t\033[91m30)\033[0m Backdoor-Factory")
+        print("")
+        print("\033[91m99)\033[0m back to main menu\n")
